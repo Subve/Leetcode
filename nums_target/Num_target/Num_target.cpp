0018889e8f5ef8695ba80e9a5612d759fd6d8136
Subve <miloszgorny7@gmail.com>
2022-07-08 19:44:26 +0200

    vectors reminding

---
+++ nums_target/Num_target/Num_target.cpp
@@ -0,0 +1,60 @@
+﻿// Num_target.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+
+using namespace std;
+
+
+//class solution {
+//public:
+//    
+//    vector<int> twosum(vector<int>& nums, int target) {
+//        int s = size(nums);          // count the length of the 
+//        for (int i = 0; i < s; i++) {            //check each element in array
+//            for (int j = i + 1; j < s; j++) {            //check each other element in the array
+//                if (nums[i] + nums[j] == target) {         //determine if these two elements sum to target
+//                    return{ i, j };       //return all pairs of integers
+//                }
+//            }
+//        }
+//        return { -1, -1 };
+//    
+//
+//};
+
+int main()
+{
+    vector<int> wholeNumbers;
+    wholeNumbers.push_back(20);
+    wholeNumbers.push_back(-45);
+
+    cout << wholeNumbers[0] << endl;
+    cout << wholeNumbers[1] << endl;
+    wholeNumbers.push_back(200);
+    cout << wholeNumbers.back() << endl;
+    wholeNumbers.push_back(200);
+    wholeNumbers.push_back(200);
+    cout << wholeNumbers.back() << endl;
+    wholeNumbers.push_back(200);
+    cout << wholeNumbers.back() << endl;
+    cout << wholeNumbers.back() << endl;
+    cout << wholeNumbers.size() << endl;
+    cout << wholeNumbers.capacity();
+    cout << "..........." << endl;
+    wholeNumbers.erase(3);
+    return 0;
+}
+
+// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
+// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
+
+// Porady dotyczące rozpoczynania pracy:
+//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
+//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
+//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
+//   4. Użyj okna Lista błędów, aby zobaczyć błędy
+//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
+//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
---
+++ nums_target/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+    }
+    
+};
+int main(){
+
+    return 0;
+}
