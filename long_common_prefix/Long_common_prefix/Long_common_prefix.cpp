6b29bf02fd97ab2a6807871b7dbda55f9cb00083
Subve <miloszgorny7@gmail.com>
2022-07-11 02:29:33 +0200

    creating loop

---
+++ long_common_prefix/Long_common_prefix/Long_common_prefix.cpp
@@ -0,0 +1,36 @@
+﻿// Long_common_prefix.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+class Solution {
+public:
+   std::string longestCommonPrefix(std::vector<std::string>& strs) {
+       for (int i = 0; i < strs[0].length() - 1; i++)
+       {
+           return strs[0];
+       }
+       //return strs[0];
+    }
+};
+
+int main()
+{   
+    std::vector<std::string> aww = {"flower", "flow", "flight" };
+    Solution sol;
+    /*std::cout << aww[0];
+    std::cout << sol.longestcommonprefix(aww);
+    std::cout << aww[0];*/
+}
+
+// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
+// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
+
+// Porady dotyczące rozpoczynania pracy:
+//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
+//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
+//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
+//   4. Użyj okna Lista błędów, aby zobaczyć błędy
+//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
+//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
